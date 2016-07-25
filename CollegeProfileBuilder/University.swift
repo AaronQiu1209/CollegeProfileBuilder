0213bfbe01a986ca19834b15e3114a3fd901c346
Wei Liu <luckylw9@gmail.com>
2016-07-25 17:21:05 -0500

    added add button and created custom university class

---
+++ CollegeProfileBuilder/University.swift
@@ -0,0 +1,13 @@
+//
+//  University.swift
+//  CollegeProfileBuilder
+//
+//  Created by Aaron on 2016-07-25.
+//  Copyright © 2016 Aaron. All rights reserved.
+//
+
+import UIKit
+
+
+
+
--- CollegeProfileBuilder/ViewController.swift
+++ CollegeProfileBuilder/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var universities = ["NorthWestern", "Stanford", "Berkley",]
+    var universities = ["Berkley", "Stanford", "NorthWestern", "UPenn"]
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -25,6 +25,31 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = universities[indexPath.row]
         return cell
     }
-}
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete{
+            universities.removeAtIndex(indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    @IBAction func onTappedButtonAdd(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add University", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Add University Here"
+            
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) in
+            let universityTextField = alert.textFields![0] as UITextField
+            self.universities.append(universityTextField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addAction(addAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+
+    }
+
 
 
