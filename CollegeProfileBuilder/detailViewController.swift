81439baa58f07e669bfe6b05c867af5f178114cf
Wei Liu <luckylw9@gmail.com>
2016-07-26 14:41:20 -0500

    Added Camera and photo library, also added something to ensure the textfield will have "https://" in it.

--- CollegeProfileBuilder/detailViewController.swift
+++ CollegeProfileBuilder/detailViewController.swift
@@ -9,7 +9,7 @@
 
 import UIKit
 import SafariServices
-class detailViewController: UIViewController, SFSafariViewControllerDelegate {
+class detailViewController: UIViewController, SFSafariViewControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     @IBOutlet weak var webAdressTextField: UITextField!
     
@@ -21,6 +21,7 @@ class detailViewController: UIViewController, SFSafariViewControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     var university: University!
+    var imagePicker = UIImagePickerController()
     
     
     override func viewDidLoad() {
@@ -30,19 +31,42 @@ class detailViewController: UIViewController, SFSafariViewControllerDelegate {
         enrollmentTextField.text = String(university.enrollment)
         webAdressTextField.text = university.urlString
         imageView.image = university.image
+        imagePicker.delegate = self
     }
     @IBAction func saveButtonTapped(sender: UIButton) {
         university.uniName = universityTextField.text!
         university.location = locationTextField.text!
         university.enrollment = Int(enrollmentTextField.text!)!
         university.urlString = webAdressTextField.text!
+        university.image = imageView.image!
     }
     @IBAction func openInSafariWithDoneButton(sender: UIButton) {
-        let url = NSURL(string: university.urlString)!
-        let svc = SFSafariViewController(URL: url)
-        presentViewController(svc, animated: true, completion: nil)
+        if webAdressTextField.text!.containsString("https://") == true{
+            let url = NSURL(string: university.urlString)!
+            let svc = SFSafariViewController(URL: url)
+            presentViewController(svc, animated: true, completion: nil)
+        } else {
+            webAdressTextField.text! = "https://" + webAdressTextField.text!
+        }
+        
     }
     
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imagePicker.dismissViewControllerAnimated(true){
+            let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            self.imageView.image = selectedImage
+        }
+    }
     @IBAction func libraryOnTappedButton(sender: UIButton) {
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraOnTappedButton(sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
+            
+            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+            presentViewController(imagePicker, animated: true, completion: nil)
+        }
     }
 }
